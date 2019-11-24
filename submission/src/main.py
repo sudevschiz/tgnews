ed727f65d08a16c5bef102c210736eb8c0a72cc2
Sudev Chirappat <sudevchirappat@Sudevs-MacBook-Pro.local>
2019-11-24 11:48:50 +0900

    Category classifier added

---
+++ submission/src/main.py
@@ -0,0 +1,8 @@
+import sys
+import languages
+
+
+if __name__ == "__main__":
+    if sys.argv[1] == "languages":
+        print("to task 1")
+        print("separating english and russian in folder: "+sys.argv[2])
